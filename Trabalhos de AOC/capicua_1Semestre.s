548f02c7f107290e7931c7186c93484e1b1e33ed
José Pozzato <130502687+pozzatojr@users.noreply.github.com>
2026-05-12 15:48:09 -0300

    Update capicua_1Semestre.s
    Agora esta pronto para ser entregue!
    Algoritmo em Assembly, o qual lê um número (1 < número < 10000 e, caso não seja, deve ser novamente solicitado.) e caso ele não seja um número palíndromo (capicua), apontar qual o número palíndromo (capicua) mais próximo a seguir dele.

--- Trabalhos de AOC/capicua_1Semestre.s
+++ Trabalhos de AOC/capicua_1Semestre.s
@@ -11,42 +11,49 @@ main:
 	
 	li $v0, 5
 	syscall
+	
 	add $t0, $v0, 0 #t0 = valor digitado
 	add $t3, $t0, 0
+	add $t4, $t0, 0
 	
 	ble $t3, 1, main
 	bge $t3, 10000, main
-	
-start:	
-	rem $t1, $t0, 10 #t1 = resto
-	
-	div $t0, $t0, 10
-	
-	mul $t2, $t2, 10
-	add $t2, $t2, $t1
-	
-	bgt $t0, 0, start
-	beq $t2, $t3, fim #verifica se ´e palindromo
-	j erro
 
-erro:
-	add $t3, $t3, 1
-	add $t0, $t3, 0
-	li $t2, 0
+startLoop:	
+	rem $t1, $t0, 10 #t1 = resto
+	div $t0, $t0, 10 #div por 10	
+	mul $t2, $t2, 10 # começo da formula
+	add $t2, $t2, $t1 #restante da formula
 	
+	bgt $t0, 0, startLoop
+	beq $t2, $t3, capicua #verifica se ´e palindromo
+	j naoCapicua
+
+naoCapicua:
+	add $t3, $t3, 1
+	add $t0, $t3, 0	
+	li $t2, 0
+		
+	j startLoop
+	
+fimCapicua:
 	li $v0, 4
 	la $a0, msg2
 	syscall
 	
-	j start
+	li $v0, 1
+	add $a0, $t2, 0
+	syscall
+	j end
 	
-
-fim:		
+capicua:	
+	blt $t4, $t3, fimCapicua
+		
 	li $v0, 1
 	add $a0, $t2, 0 #mostrando o res da formula
 	syscall
 	
 	li $v0, 4
 	la $a0, msg3
-	syscall
-		
+	syscall		
+end:
